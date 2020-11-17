81e75f1ae3ce8889d184784f7f4e3378b55238f2
kicbar <bartlomiejkicior@gmail.com>
2020-11-17 23:59:50 +0100

    add controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Komis.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Komis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISamochodRepository _samochodRepository;
+
+        public HomeController(ISamochodRepository samochodRepository)
+        {
+            _samochodRepository = samochodRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
