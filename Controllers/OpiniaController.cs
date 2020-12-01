a14c496725baa6c2f3ae4f18e4d23107bac73058
kicbar <bartlomiejkicior@gmail.com>
2020-12-01 20:57:30 +0100

    add authorization

--- Controllers/OpiniaController.cs
+++ Controllers/OpiniaController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Komis.Models;
+﻿using Komis.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Komis.Controllers
 {
+    [Authorize]
     public class OpiniaController : Controller
     {
         private readonly IOpiniaRepository _opiniaRepository;
